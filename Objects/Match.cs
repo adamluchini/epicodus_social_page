ab0a68a76f544e68c6d0fdf80da7eb1f3b4ba164
Adam Luchini and bo zhao <adam+bo@>
2016-07-27 14:22:48 -0700

    include data in tables

--- Objects/Match.cs
+++ Objects/Match.cs
@@ -6,29 +6,9 @@ namespace Codex.Objects
 {
   public class Match
   {
-    public static List<Profile> MatchMBs(Profile p, string choice)
-    {
-      List<Profile> allProfiles = Profile.GetAll();
+    public static List<Profile> MatchMBs(Profile p, string choice,
 
-      List<Profile> resultPerfect = new List<Profile> {};
-      List<Profile> resultGood = new List<Profile> {};
-      foreach(var profile in allProfiles)
-      {
-        if(profile.id != p.id)
-        {
-          if(Match.MatchMB(p, profile) == "perfectly"){
-            resultPerfect.Add(profile);
-          }
-          if(Match.MatchMB(p, profile) == "good"){
-            resultGood.Add(profile);
-          }
-        }
-      }
-      if(choice == "perfect")  return resultPerfect;
-      else return resultGood;
-    }
-
-    public static string MatchMB(Profile P1, Profile P2)
+    delegate string MatchMB(Profile P1, Profile P2)
     {
       int counter = 0;
       if(P1.ei == P2.ei)
@@ -41,6 +21,42 @@ namespace Codex.Objects
       else if(counter==2) return "good";
       else return "no match";
     }
+
+    )
+    {
+      List<Profile> allProfiles = Profile.GetAll();
+
+      List<Profile> resultPerfect = new List<Profile> {};
+      List<Profile> resultGood = new List<Profile> {};
+      foreach(var profile in allProfiles)
+      {
+        if(profile.id != p.id)
+        {
+          if(MatchMB(p, profile) == "perfectly"){
+            resultPerfect.Add(profile);
+          }
+          if(MatchMB(p, profile) == "good"){
+            resultGood.Add(profile);
+          }
+        }
+      }
+      if(choice == "perfect")  return resultPerfect;
+      else return resultGood;
+    }
+
+    // public delegate string MatchMB(Profile P1, Profile P2)
+    // {
+    //   int counter = 0;
+    //   if(P1.ei == P2.ei)
+    //     counter++;
+    //   if(P1.ns == P2.ns && P1.tf == P2.tf)
+    //     counter++;
+    //   if(P1.pj == P2.pj)
+    //     counter++;
+    //   if(counter==3) return "perfectly";
+    //   else if(counter==2) return "good";
+    //   else return "no match";
+    // }
 
   }
 }
